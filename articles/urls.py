aa1807601ed73888eb9e80654a5c3c47c7dec778
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-05-17 16:01:06 +0100

    Add images

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.article_list),
+    url(r'^$', views.article_list, name='list'),
     url(r'^about/$', views.article_list),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_details, name='details'),
 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
 
 # Create your views here.
@@ -7,3 +8,9 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by("date")
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_details(request, slug):
+    #  return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_details.html', {'article': article})
