1769ac84c9689a44bb0066197ea1db437bef04dd
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-05-19 19:03:45 +0100

    Add forms nad style

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ app_name = 'articles'
 
 urlpatterns = [
     url(r'^$', views.article_list, name='list'),
-    url(r'^about/$', views.article_list),
+    url(r'^create/$', views.article_create, name='create'),
     url(r'^(?P<slug>[\w-]+)/$', views.article_details, name='details'),
 
 ]
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,13 +19,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from articles import views as article_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^articles/', include('articles.urls')),
     url(r'^about/$', views.about),
-    url(r'^$', views.homepage),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^$', article_views.article_list, name='home'),
 
 
 ]
